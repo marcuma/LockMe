8b5fccad89a0cd7b512b74588c2d7581a744b29e
Tony Marcum <marcuma@gmail.com>
2021-08-22 17:57:00 -0400

    Adding Initial Project Structure

---
+++ src/com/simplilearn/lockme/application/Application.java
@@ -0,0 +1,13 @@
+package com.simplilearn.lockme.application;
+
+/**
+ * Created: 08/22/2021 - 5:54 PM
+ * Project: LockMe
+ *
+ * @author marcuma
+ */
+public class Application {
+    public static void main(String[] args) {
+
+    }
+}
